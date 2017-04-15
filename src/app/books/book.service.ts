cdf09839bd863be2839e39eb06fa9f1ee547fdfc
pawel.idziak <208766@student.pwr.edu.pl>
2017-04-15 12:05:18 +0200

    poprawki w url do plikow

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -15,9 +15,8 @@ export class BookService {
 
   getBooks(): Observable<IBook[]> {
     return this._http
-      .get('../../assets/books.json')
+      .get('././assets/books.json')
       .map((response: Response) => <IBook[]> response.json())
-      // .do(data => console.log(data))
       .catch(this.handleError);
   }
 
